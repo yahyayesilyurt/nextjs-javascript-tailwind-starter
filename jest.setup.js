bfdfa8947bda8ea398bb110e8935fd2cbcdd7490
yahyayesilyurt <yesilyurty21@itu.edu.tr>
2023-09-06 15:23:01 +0300

    feat: init

---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+// eslint-disable-next-line import/no-unresolved
+import '@testing-library/jest-dom/extend-expect';
---
+++ src/pages/_app.js
@@ -0,0 +1,6 @@
+import '../styles/globals.css';
+import { appWithTranslation } from 'next-i18next';
+
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+export default appWithTranslation(MyApp);
